2ed0fef6c968f3d44f0e01329051adda1cdeaa51
Igor <igorozani.gmail.com>
2020-05-28 18:07:22 -0300

    add mongoUri

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoUri: "mongodb+srv://igorozani:IguIn1995@cluster0-jar0r.gcp.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports = {mongoUri: "mongodb+srv://igorozani:UQ97B6MUgMhWed1k@cluster0-jar0r.gcp.mongodb.net/test?retryWrites=true&w=majority"}
 } else {
     module.exports = {mongoUri: "mongodb://localhost/estoque"}
 }
