eb93bd443d672dfb006073f69191e1bc0cd3ab1f
Igor <igorozani.gmail.com>
2020-05-29 03:31:25 -0300

    add mongoUri

--- index.js
+++ index.js
@@ -85,6 +85,6 @@ const ProdutoVendido = require('./models/ProdutoVendido')
 // Outros
     const PORT = process.env.PORT || 3000
     app.listen(PORT, () =>{
-        console.log('Servidor rodando na porta 3000');
+        console.log('Servidor rodando na porta ' + PORT);
     })
 
